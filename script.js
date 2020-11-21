a567cd67c746302a5ca4932e7d22350c0b9f215f
Andrey <example@gmail.com>
2020-11-21 18:15:01 +0400

    Answers for create movies obj

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 'use strict';
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-let lastWathcingFilm = prompt('Один из последних просмотренных фильмов');
-let lastWatchingFilmRate = prompt('На сколько его оцените?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
-    count: +numberOfFilms,
-    movies: {
-        [lastWathcingFilm]: lastWatchingFilmRate
-    },
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-console.log(personalMovieDB)
+
+const a = prompt('Один из последних просмотренных фильмов'),
+      b = prompt('На сколько его оцените?'),
+      c = prompt('Один из последних просмотренных фильмов'),
+      d = prompt('На сколько его оцените?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
