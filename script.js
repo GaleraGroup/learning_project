9139e3bb44a64c0bc3b416f0122b8abf163146e4
Andrey <example@gmail.com>
2020-11-21 17:35:53 +0400

    two questions

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 'use strict';
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let lastWathcingFilm = prompt('Один из последних просмотренных фильмов');
+let lastWatchingFilmRate = prompt('На сколько его оцените?');
+
 let personalMovieDB = {
     count: +numberOfFilms,
-    movies: {},
+    movies: {
+        lastWathcingFilm: lastWatchingFilmRate
+    },
     actors: {},
     genres: [],
     privat: false
 }
+
+
